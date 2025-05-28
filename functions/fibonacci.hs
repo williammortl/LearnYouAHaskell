ee8d7eaa55ffe3c7c195493dcebd3a8154bbe3d4
William Mortl <wmortl@Defiant.local>
2025-05-28 15:50:57 -0600

    more progress, guards

--- functions/fibonacci.hs
+++ functions/fibonacci.hs
@@ -3,17 +3,21 @@ module Main where
 import Data.List
 import System.IO
 import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 fibonacci :: (Integral a) => a -> a
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = (fibonacci (n - 2)) + (fibonacci (n - 1)) 
+fibonacci n
+    | n == 0    = 0
+    | n == 1    = 1
+    | otherwise = (fibonacci (n - 2)) + (fibonacci (n - 1)) 
+
+usage = "\nUsage:\n\nThis program calculates the nth fibonacci number using recursion.\n\n\t./fibonacci {what number to calculate fibonacci number of}\n"
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [x] -> do
-            let number = read x :: Int
-            putStrLn ("Fibonacci of " ++ show number ++ " is: " ++ show (fibonacci number))
-        _   -> putStrLn "\nUsage:\n\nThis program calculates the nth fibonacci number using recursion.\n\n\t./fibonacci {what number to calculate fibonacci number of}\n"    
+        [x] -> case readMaybe x :: Maybe Int of
+            Just number -> putStrLn ("Fibonacci of " ++ show number ++ " is: " ++ show (fibonacci number))
+            Nothing     -> putStrLn usage
+        _   -> putStrLn usage
